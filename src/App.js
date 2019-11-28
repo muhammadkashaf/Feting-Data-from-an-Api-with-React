34fa454ea1f62e2a6de2f0de0e5145fefd256cb6
kashaf ahmed <kashafahmed72@gmail.com>
2019-11-29 03:20:24 +0500

    feting data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [],
+      isLoaded: false,
+
+    }
+  }
+
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+
+
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      });
+  }
+
+
+  render() {
+
+    const { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading......</div>;
+    }
+
+    else {
+
+      return (
+
+        < div className="App" >
+
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                Names: {item.name} <br /> Emails: {item.email} <br /><br />
+              </li>
+            ))};
+        </ul>
+
+        </div>
+      );
+    }
+
+  }
+
 }
+
 
 export default App;
